4176438d2448d6b571a99501b117ab11def9540d
tinghsu <tinghsu@kronosresearch.com>
2021-07-23 06:36:57 +0000

    Import

--- tests/test_ws.py
+++ tests/test_ws.py
@@ -1,7 +1,12 @@
 import pytest
 from wootrade.clinet import Client
+from wootrade import ThreadedWebsocketManager
 import os
 
 API = os.getenv("API")
 SECRET = os.getenv("SECRET")
 APPLICATION_ID = os.getenv("APPLICATION_ID")
+
+
+def test_create_TWM():
+    wsm = ThreadedWebsocketManager(API, SECRET, APPLICATION_ID, False)
