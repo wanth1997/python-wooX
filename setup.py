5139ea85bc433461dbc03e4bce6f9c8e3f4ec93d
tinghsu <tinghsu@kronosresearch.com>
2021-07-23 07:33:53 +0000

    New setup

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
-from setuptools import setup
+from __future__ import print_function
+from setuptools import setup, find_packages
+import sys
 
 setup(
-    name="test_tox",
-    script=["test_api"],
+    name="python-wootrade",
+    version="0.1.0",
+    author="tinghsuwan",
+    author_email="wanth1997@gmail.com",
+    description="Wootrade python API",
+    license="MIT",
+    url="https://github.com/wanth1997/python-wootrade",
+    packages=["wootrade"],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests", "aiohttp", "websockets"],
+    zip_safe=True,
 )
