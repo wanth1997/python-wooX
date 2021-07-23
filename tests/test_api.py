c39a12eb079ffa9dbb8a5583cfbad14f305dd4bf
tinghsu <tinghsu@kronosresearch.com>
2021-07-23 04:01:46 +0000

    Remove useless output

--- tests/test_api.py
+++ tests/test_api.py
@@ -17,7 +17,6 @@ def test_create_client():
 def test_get_exchange_info():
     client = Client(API, SECRET, APPLICATION_ID, testnet=True)
     info = client.get_exchange_info(symbol="SPOT_BTC_USDT")
-    print(API)
     assert "success" in info and info["success"] == True
     assert "info" in info
 
---
+++ tests/test_ws.py
@@ -0,0 +1,7 @@
+import pytest
+from wootrade.clinet import Client
+import os
+
+API = os.getenv("API")
+SECRET = os.getenv("SECRET")
+APPLICATION_ID = os.getenv("APPLICATION_ID")
