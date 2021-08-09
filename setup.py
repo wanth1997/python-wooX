e78120dabfccfcd5130bac95363bb7528178531f
tinghsu <tinghsu@kronosresearch.com>
2021-08-09 04:12:57 +0000

    Update tag

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup(
     name="python-wootrade",
-    version="0.1.2",
+    version="0.1.3",
     author="tinghsuwan",
     author_email="wanth1997@gmail.com",
     description="Wootrade python API",
