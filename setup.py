94a7512444f909e1c84806ec49918aa6283a306b
tinghsu <wanth1997@gmail.com>
2024-04-23 00:02:26 +0800

    Rename

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup(
     name="python-woox",
-    version="0.2.0",
+    version="0.2.1",
     author="tinghsuwan",
     author_email="wanth1997@gmail.com",
     description="WooX python API",
